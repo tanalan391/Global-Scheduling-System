3de70b44ad5ec088ae37eea778de2ec8a309c9db
tanalan391 <ttolma4@wgu.edu>
2023-03-13 11:42:37 -0600

    3.13.2

---
+++ src/main/main.java
@@ -0,0 +1,32 @@
+package main;
+
+import DAO.CustDAO;
+import DAO.DBQueries;
+import DAO.DBC;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.sql.SQLException;
+
+public class main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/view/Login.fxml"));
+        primaryStage.setTitle("Global Scheduling System");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) throws SQLException {
+       /* DBC.startConnection();
+        DBQueries.getAll();
+        //launch(args);
+        DBC.closeConnection();
+        */
+    }
+}
